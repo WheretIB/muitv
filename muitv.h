48d0291022fdcd26150807dd2adc8f9940928389
WheretIB <wheretib@gmail.com>
2015-10-10 17:59:03 +0300

    Added option to override C memory allocation functions with functions from muitv (x86-only)

--- muitv.h
+++ muitv.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cstdint>
+#include <cstdlib>
 
 extern "C" __declspec(dllimport) void* muitv_alloc(size_t size);
 extern "C" __declspec(dllimport) void* muitv_calloc(size_t count, size_t size);
@@ -27,3 +28,39 @@ void operator delete[](void* ptr)
 {
 	muitv_free(ptr);
 }
+
+#if defined(OVERRIDE_CLIB_MALLOC)
+extern "C" _declspec(dllimport) int __stdcall VirtualProtect(void* lpAddress, size_t dwSize, unsigned long flNewProtect, unsigned long* lpflOldProtect);
+
+namespace muitv
+{
+	static inline void patch_with_jump(void* dest, void* address)
+	{
+		// get offset for relative jmp
+		unsigned int offset = (unsigned int)((char*)address - (char*)dest - 5);
+
+		// unprotect memory
+		unsigned long old_protect;
+		VirtualProtect(dest, 5, 0x04, &old_protect);
+
+		// write jmp
+		*(unsigned char*)dest = 0xe9;
+		*(unsigned int*)((char*)dest + 1) = offset;
+
+		// protect memory
+		VirtualProtect(dest, 5, old_protect, &old_protect);
+	}
+
+	unsigned muitv_patch_clib_functions()
+	{
+		patch_with_jump((void*)malloc, (void*)muitv_alloc);
+		patch_with_jump((void*)calloc, (void*)muitv_calloc);
+		patch_with_jump((void*)realloc, (void*)muitv_realloc);
+		patch_with_jump((void*)free, (void*)muitv_free);
+
+		return 0;
+	}
+
+	static const unsigned muitv_patch_clib_functions_ready = muitv_patch_clib_functions();
+}
+#endif
